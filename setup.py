158e0113af0dc411caa450d9de7232610086d448
Blank <94945186+Blank-c@users.noreply.github.com>
2022-06-13 11:59:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
    url='https://github.com/Blank-c/NekosLife',
    description='An unofficial wrapper for nekos.life api with proxy support.',
    long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
    install_requires=['aiohttp'],
    keywords='nekoslife'
 )
