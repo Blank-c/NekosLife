c07c9f6b4627dc1597cec3dca93c79b4aad85be1
Blank <94945186+Blank-c@users.noreply.github.com>
2022-06-13 11:45:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
    name='nekoslife',
    version='0.0.1',
-   author='Blank',
+   author='Blank-c',
    packages=find_packages(),
-   license='LICENSE',
    url='https://github.com/Blank-c/NekosLife',
    description='An unofficial wrapper for nekos.life api with proxy support.',
    long_description=open('README.md').read(),
